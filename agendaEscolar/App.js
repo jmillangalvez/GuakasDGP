999acacc8def274f79f11bc3af1e1878f79b8446
Jaime Millan <jmillangalvez@gmail.com>
2021-11-04 19:39:58 +0100

    inicio de sesion empezado

--- agendaEscolar/App.js
+++ agendaEscolar/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SafeAreaView style={styles.banner}>
+        <Text style={styles.headerText}>ENTRAR</Text>
+      </SafeAreaView>
+      <View style={styles.buttonChoose}>
+        <Button title="ELEGIR ALUMNO" color="#faff6d">
+        </Button>
+      </View>
+      <View style={styles.buttonView}>
+        <Button title="ENTRAR" color="#faff6d" style={styles.enterButton}>
+        </Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +23,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+  },
+  banner: {
+    backgroundColor: '#248aff',
+    width: '100%',
+    height: '15%',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  headerText: {
+    fontSize: 70,
+    fontWeight: 'bold'
+  },
+  enterButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonView: {
+    width: '20%'
+  },
+  buttonChoose: {
+    width: '20%'
   },
 });
