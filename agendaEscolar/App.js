2b1d1e344a97d41528a1487fd94a6a33204dd50d
Jesús Espínola Conde <jesusespinolaconde@gmail.com>
2021-11-10 13:10:50 +0100

    Final tutorial tareas

--- agendaEscolar/App.js
+++ agendaEscolar/App.js
@@ -7,7 +7,6 @@ import { Tasks } from './Tasks'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <Tasks />
       <StatusBar style="auto" />
     </View>
--- agendaEscolar/Tasks.js
+++ agendaEscolar/Tasks.js
@@ -1,31 +1,60 @@
-import React, { Fragment, useState, useRef } from "react";
-
+import React, { Fragment, useState, useRef, useEffect } from "react";
 import { TodoList } from "./components/TodoList";
+
+const KEY ="todoApp.todos";
 
 export function Tasks() {
     const [todos, setTodos] = useState([
-        { id: 1, task: "Tarea 1", completed: false}
+        { id: 0, task: "Tarea 1", completed: false}
     ]);
 
     const todoTaskRef = useRef();
+    const idTaskRef = useRef();
+
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(KEY));
+        
+        if (storedTodos) {
+            setTodos(storedTodos);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(KEY, JSON.stringify(todos));
+    }, [todos]);
+
+    const toggleTodo = (id) => {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id == id);
+        todo.completed = !todo.completed;
+        setTodos(newTodos);
+    };
 
     const handleTodoAdd = () => {
         const task = todoTaskRef.current.value;
+        const id = idTaskRef.current.value;
         if (task == '') return;
 
         setTodos((prevTodos) => {
-            return [...prevTodos, {id: 0, task, completed: false}]
+            return [...prevTodos, {id, task, completed: false}]
         });
 
         todoTaskRef.current.value = null;
     };
 
+    const handleClearAll = () => {
+        const newTodos = todos.filter((todo) => !todo.completed);
+        setTodos(newTodos);
+    };
+
     return (
         <Fragment>
-            <TodoList todos={todos} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
             <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea" />
+            <input ref={idTaskRef} type="text" placeholder="Id de la Nueva Tarea" />
             <button onClick={handleTodoAdd}>Añadir</button>
-            <button>Eliminar</button>
+            <button onClick={handleClearAll}>Eliminar Completadas</button>
+            <div>Te quedan {todos.filter((todo) => !todo.completed).length}</div>
         </Fragment>
     );
 }
