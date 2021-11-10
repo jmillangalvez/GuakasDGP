292aae6630db58c1714cc787142988778c4040df
Jaime Millan <jmillangalvez@gmail.com>
2021-11-10 13:05:10 +0100

    Prototipo funcional terminado

--- agendaEscolar/App.js
+++ agendaEscolar/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from "react";
 import { StyleSheet, Text, SafeAreaView, TouchableOpacity, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+async function changeScreenOrientation() {
+  await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { choosingStudent: false, studentChosen: false, entering: false};
+    this.state = { choosingStudent: false, studentChosen1: false, studentChosen2: false, studentChosen3: false, studentChosen4: false, entering: false};
   }
 
   chooseStudent = () => {
@@ -14,15 +19,36 @@ class App extends Component {
     });
   };
 
-  selectStudent = () => {
+  selectStudent1 = () => {
     this.setState({
-      studentChosen: true,
+      studentChosen1: true,
+      choosingStudent: false
+    });
+  };
+
+  selectStudent2 = () => {
+    this.setState({
+      studentChosen2: true,
+      choosingStudent: false
+    });
+  };
+
+  selectStudent3 = () => {
+    this.setState({
+      studentChosen3: true,
+      choosingStudent: false
+    });
+  };
+
+  selectStudent4 = () => {
+    this.setState({
+      studentChosen4: true,
       choosingStudent: false
     });
   };
 
   enter = () => {
-    if (this.state.studentChosen) {
+    if (this.state.studentChosen1 || this.state.studentChosen2 || this.state.studentChosen3 || this.state.studentChosen4) {
       this.setState({
         entering: true
       });
@@ -31,15 +57,25 @@ class App extends Component {
 
   choosingStudentView(){
     return (
-      <View style={styles.buttonChoose}>
-        <TouchableOpacity style={styles.buttonChoose} onPress={this.selectStudent}>
+      <View style={styles.choosingView}>
+        <TouchableOpacity style={styles.choosingButton1} onPress={this.selectStudent1}>
           <Text style={styles.buttonText}>ALUMNO 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.choosingButton2} onPress={this.selectStudent2}>
+          <Text style={styles.buttonText}>ALUMNO 2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.choosingButton3} onPress={this.selectStudent3}>
+          <Text style={styles.buttonText}>ALUMNO 3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.choosingButton4} onPress={this.selectStudent4}>
+          <Text style={styles.buttonText}>ALUMNO 4</Text>
         </TouchableOpacity>
       </View>
     );
-  }
+  } 
 
   enteringView(){
+    this
     return (
       <View style={styles.buttonChoose}>
         <Text style={styles.buttonText}>ENTRANDO</Text>
@@ -47,38 +83,48 @@ class App extends Component {
     );
   }
 
-  chooseStudentButton(){
+  chooseStudentView(){
     return(
-      <TouchableOpacity style={styles.buttonChoose} onPress={this.chooseStudent}>
-        <Text style={styles.buttonText}>ELEGIR</Text>
-        <Text style={styles.buttonText}>ALUMNO</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonChoose}>
+        <TouchableOpacity style={styles.buttonChoose} onPress={this.chooseStudent}>
+          <Text style={styles.buttonText}>ELEGIR</Text>
+          <Text style={styles.buttonText}>ALUMNO</Text>
+        </TouchableOpacity>
+      </View>
     )
   }
 
-  studentChosenText(){
+  studentChosenView(num){
     return(
-      <Text style={styles.buttonText}>ALUMNO{"\n"}ELEGIDO</Text>
+      <View style={styles.buttonChoose}>
+        <TouchableOpacity style={styles.buttonChoose}>
+          <Text style={styles.buttonText}>ALUMNO {num}{"\n"}ELEGIDO</Text>
+        </TouchableOpacity>
+      </View>
     )
   }
+  
 
   render() {
-    const { choosingStudent, studentChosen, entering } = this.state;
+    const { choosingStudent, studentChosen1, studentChosen2, studentChosen3, studentChosen4, entering } = this.state;
+    changeScreenOrientation();
     return (
       <View style={styles.container}>
         <SafeAreaView style={styles.banner}>
           <Text style={styles.headerText} value="ENTRAR">ENTRAR</Text>
         </SafeAreaView>
-        <View style={styles.buttonChoose}>
-          {studentChosen? this.studentChosenText() : this.chooseStudentButton()}
-        </View>
+        {!choosingStudent && !(studentChosen1 || studentChosen2 || studentChosen3 || studentChosen4)? this.chooseStudentView() : null}
+        {choosingStudent? this.choosingStudentView() : null}
+        {studentChosen1? this.studentChosenView("1") : null}
+        {studentChosen2? this.studentChosenView("2") : null}
+        {studentChosen3? this.studentChosenView("3") : null}
+        {studentChosen4? this.studentChosenView("4") : null}
         <View style={styles.buttonView}>
           <TouchableOpacity style={styles.buttonView} onPress={this.enter}>
             <Text style={styles.buttonText}>ENTRAR</Text>
           </TouchableOpacity>
         </View>
-        {choosingStudent? this.choosingStudentView() : null}
-        {entering && studentChosen? this.enteringView() : null}
+        {entering && (studentChosen1 || studentChosen2 || studentChosen3 || studentChosen4)? null : null}
         <StatusBar style="auto" />
       </View>
     );
@@ -105,6 +151,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   buttonView: {
+    marginTop: '10%', 
     width: 250,
     height: 70,
     backgroundColor: "#faff6d",
@@ -112,15 +159,69 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   buttonChoose: {
+    marginTop: '8%',
     width: 150,
     height: 150,
     backgroundColor: "#faff6d",
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonText: {
+  choosingView: {
+    marginTop: '8%',
+    width: '80%',
+    height: 150,
+    backgroundColor: "#faff6d",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  choosingButton1:  {
+    height: '100%',
+    width: '15%',
+    borderWidth: 3,
     fontSize: 25,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    position: 'absolute',
+    marginLeft: '-70%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  choosingButton2:  {
+    height: '100%',
+    width: '15%',
+    borderWidth: 3,
+    fontSize: 25,
+    fontWeight: 'bold',
+    position: 'absolute',
+    marginLeft: '-23%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  choosingButton3:  {
+    height: '100%',
+    borderWidth: 3,
+    width: '15%',
+    fontSize: 25,
+    fontWeight: 'bold',
+    position: 'absolute',
+    marginLeft: '23%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  choosingButton4:  {
+    height: '100%',
+    width: '15%',
+    borderWidth: 3,
+    fontSize: 25,
+    fontWeight: 'bold',
+    position: 'absolute',
+    marginLeft: '70%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: 'bold',
   },
   choosingStudentView: {
     width: 250,
