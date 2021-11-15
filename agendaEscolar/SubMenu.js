08d722a3d6c4f89218ff4acd1e5529c5da969852
jmnavas3 <joselillo.sk8@gmail.com>
2021-11-15 14:29:33 +0100

    modificados nombres estilos

--- agendaEscolar/SubMenu.js
+++ agendaEscolar/SubMenu.js
@@ -6,27 +6,27 @@ function subMenu() {
   return (
     <View style={styles.container}>
       <View style={styles.goBackView}>
-        <TouchableOpacity onPress={() => alert('Adios')}>
+        <TouchableOpacity onPress={() => navigation.navigate('submenu')}>
           <Text style={styles.goBackText}>Volver</Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.enterButtonTouch} onPress={() => navigation.navigate('submenu')}>
-        <Text style={styles.buttonText}>Añadir tarea fija</Text>
+      <TouchableOpacity style={styles.taskButtonTouch} onPress={() => navigation.navigate('submenu')}>
+        <Text style={styles.taskButtonText}>Añadir tarea fija</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.enterButtonTouch} onPress={() => navigation.navigate('submenu')}>
-        <Text style={styles.buttonText}>Añadir comanda comedor</Text>
+      <TouchableOpacity style={styles.taskButtonTouch} onPress={() => navigation.navigate('submenu')}>
+        <Text style={styles.taskButtonText}>Añadir comanda comedor</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.enterButtonTouch} onPress={() => navigation.navigate('submenu')}>
-        <Text style={styles.buttonText}>Añadir comanda stock</Text>
+      <TouchableOpacity style={styles.taskButtonTouch} onPress={() => navigation.navigate('submenu')}>
+        <Text style={styles.taskButtonText}>Añadir comanda stock</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.enterButtonTouch} onPress={() => navigation.navigate('submenu')}>
-        <Text style={styles.buttonText}>Modificar tarea fija</Text>
+      <TouchableOpacity style={styles.taskButtonTouch} onPress={() => navigation.navigate('submenu')}>
+        <Text style={styles.taskButtonText}>Modificar tarea fija</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.enterButtonTouch} onPress={() => navigation.navigate('submenu')}>
-        <Text style={styles.buttonText}>Modificar comanda comedor</Text>
+      <TouchableOpacity style={styles.taskButtonTouch} onPress={() => navigation.navigate('submenu')}>
+        <Text style={styles.taskButtonText}>Modificar comanda comedor</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.enterButtonTouch} onPress={() => navigation.navigate('submenu')}>
-        <Text style={styles.buttonText}>Modificar comanda stock</Text>
+      <TouchableOpacity style={styles.taskButtonTouch} onPress={() => navigation.navigate('submenu')}>
+        <Text style={styles.taskButtonText}>Modificar comanda stock</Text>
       </TouchableOpacity>
     </View>
   );
